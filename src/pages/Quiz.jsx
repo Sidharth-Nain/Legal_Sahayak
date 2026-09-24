f91bf290cb7e9b37ce25bbdbdca10fe684978d85
shresthajain512-cmd <shresthajain512@gmail.com>
2026-09-24 13:55:57 +0530

    Update Quiz.jsx

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -9,10 +9,11 @@ function shuffle(arr) {
   const a = [...arr]
   for (let i = a.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
-    ;[a[i], a[j]] = [a[j], a[i]]
+      ;[a[i], a[j]] = [a[j], a[i]]
   }
   return a
 }
+// some changews to test this
 
 export default function Quiz({ focusConceptId }) {
   const { t, pick } = useLang()
